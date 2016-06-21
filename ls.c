78681d39787c37a547ef072c9273565f28f26ce7
saxsoares <sax.soares@gmail.com>
2016-06-21 22:54:05 +0000

    asdasd

--- ls.c
+++ ls.c
@@ -40,21 +40,30 @@ int main(int argc, char **argv) {
     if (n < 0)
         perror("scandir()");
     else {
-        i = 0;
-        while (i < n) {
-            printf("%s", namelist[i]->d_name);
-            print_type_name(namelist[i]->d_type);
-            stat(namelist[i]->d_name, &buf);
-            print_stat(buf);
-                free(namelist[i]);
+        while(1){
+            n = scandir("./PASTA", &namelist, NULL, alphasort);
+            m = scandir("BACKUP_PASTA", &backup_list, NULL, alphasort);
 
-            printf("BACKUP-->%s", backup_list[i]->d_name);
-            print_type_name(backup_list[i]->d_type);
-            stat(backup_list[i]->d_name, &backup_buf);
-            print_stat(backup_buf);
-                free(backup_list[i]);
+            if(m != n){
+                return 0;
+            }
+            i = 0;
+            while (i < n) {
+                printf("%s", namelist[i]->d_name);
+                print_type_name(namelist[i]->d_type);
+                stat(namelist[i]->d_name, &buf);
+                print_stat(buf);
+                    free(namelist[i]);
 
-	    i++;
+                printf("BACKUP-->%s", backup_list[i]->d_name);
+                print_type_name(backup_list[i]->d_type);
+                stat(backup_list[i]->d_name, &backup_buf);
+                print_stat(backup_buf);
+                    free(backup_list[i]);
+
+            i++;
+            }
+            sleep(1);
         }
         free(namelist);
         free(backup_list);
